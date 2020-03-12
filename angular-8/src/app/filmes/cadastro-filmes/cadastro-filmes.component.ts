c3e3d6807c59d854353c22de3460cd3e86b8f1f5
Wellington <wellingtontkd.dev@gmail.com>
2020-03-12 12:29:49 -0300

    Desabilitada validacao padrao do html 5 para o formulário  de cadastro. Alterado componente de input select para recebre dinamicamente a lista de opções

--- angular-8/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ angular-8/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -10,6 +10,7 @@ import { ValidarCamposService } from 'src/app/shared/components/campos/validar-c
 export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
+  generos: Array<string>;
   
   constructor(public validacao: ValidarCamposService,
               private fb: FormBuilder) { }
@@ -29,6 +30,7 @@ export class CadastroFilmesComponent implements OnInit {
       urlIMDb: ['', [Validators.minLength(10)]],
       genero: ['',[Validators.required]]
     });
+    this.generos = ['Ação','Aventura','Fição Cientifica', 'Drama', 'Terror', 'Comédia', 'Suspense', 'Policial', 'Romance'];
   }
 
   salvar(): void{
